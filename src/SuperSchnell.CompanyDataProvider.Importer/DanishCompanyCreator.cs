c6c33f48154232c13d430a66680cb47ceb537fe1
Goblin <duckhero@gmail.com>
2014-01-02 09:31:19 +0100

    Version 1.1
    Added data-scrubbing to importer.

--- src/SuperSchnell.CompanyDataProvider.Importer/DanishCompanyCreator.cs
+++ src/SuperSchnell.CompanyDataProvider.Importer/DanishCompanyCreator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 using LumenWorks.Framework.IO.Csv;
 using NHibernate;
@@ -12,16 +14,29 @@ namespace SuperSchnell.CompanyDataProvider.Importer
     {
         private readonly CsvReader _csvReader;
         private readonly Factory _factory = new Factory();
+        private TextInfo _textInfo;
+        private readonly Dictionary<string,string> _commonPhrases = new Dictionary<string, string>();
+
         public DanishCompanyCreator(CsvReader csvReader)
         {
             _csvReader = csvReader;
+            _textInfo = new CultureInfo("da-DK").TextInfo;
+            SetupCommonPhrases();
+        }
+
+        private void SetupCommonPhrases()
+        {
+            _commonPhrases.Add("aps","ApS");
+            _commonPhrases.Add("af","af");
+            _commonPhrases.Add("og","og");
+            _commonPhrases.Add("en","en");
         }
 
         public DanishCompany CreateNew()
         {
             var entity =_factory.CreateDanishCompany();
             entity.CVRNumber = _csvReader["cvrnr"];
-            entity.CompanyName = _csvReader["navn_tekst"];
+            entity.CompanyName = CleanString(_csvReader["navn_tekst"]);
             entity.Phone = _csvReader["telefonnummer_kontaktoplysning"];
             entity.Email = _csvReader["email_kontaktoplysning"];
             entity.Address = CreateAddress();
@@ -29,12 +44,12 @@ namespace SuperSchnell.CompanyDataProvider.Importer
         }
         private Address CreateAddress()
         {
-            return _factory.CreateAddress(CreateStreet(), _csvReader["beliggenhedsadresse_postnr"], _csvReader["beliggenhedsadresse_postdistrikt"], _csvReader["beliggenhedsadresse_bynavn"], _csvReader["beliggenhedsadresse_coNavn"]);
+            return _factory.CreateAddress(CreateStreet(), _csvReader["beliggenhedsadresse_postnr"],CleanString(_csvReader["beliggenhedsadresse_postdistrikt"]),CleanString(_csvReader["beliggenhedsadresse_bynavn"]),CleanString(_csvReader["beliggenhedsadresse_coNavn"]));
         }
 
         private string CreateStreet()
         {
-            return string.Format("{0} {1}", _csvReader["beliggenhedsadresse_vejnavn"], GetRoadNumber());
+            return string.Format("{0} {1}", CleanString(_csvReader["beliggenhedsadresse_vejnavn"]), GetRoadNumber());
         }
 
         private object GetRoadNumber()
@@ -42,7 +57,17 @@ namespace SuperSchnell.CompanyDataProvider.Importer
             var builder = new StringBuilder(string.Format("{0}{1}",_csvReader["beliggenhedsadresse_husnummerFra"], _csvReader["beliggenhedsadresse_bogstavFra"]));
             if (!string.IsNullOrEmpty(_csvReader["beliggenhedsadresse_husnummerTil"]) || !string.IsNullOrEmpty(_csvReader["beliggenhedsadresse_bogstavFra"]))
                 builder.AppendFormat("-{0}{1}", _csvReader["beliggenhedsadresse_husnummerTil"], _csvReader["beliggenhedsadresse_bogstavTil"]);
-            return builder.ToString();
+            return CleanString(builder.ToString());
+        }
+
+        private string CleanString(string convert)
+        {
+            return string.Join(" ", convert.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries).Select(s => GetProperCase(s.ToLower())));
+        }
+
+        private string GetProperCase(string convert)
+        {
+            return _commonPhrases.ContainsKey(convert) ? _commonPhrases[convert] : _textInfo.ToTitleCase(convert);
         }
     }
 }
